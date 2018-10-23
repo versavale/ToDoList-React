86f64c496b502cd8d465c51ae7c3b5db2b98bf08
Valentina Versari <versavale@gmail.com>
2018-10-23 05:43:15 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+this.state = {
+  userInput: '',
+  toDoList: []
+}
+this.handleSubmit = this.handleSubmit.bind(this);
+this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit(){
+    const itemsArray = this.state.userInput.split(',');
+    this.setState({
+      toDoList: itemsArray
+    });
+  }
+  handleChange(e){
+    this.setState({
+      userInput: e.target.value
+    });
+  }
   render() {
+    const items = this.state.toDoList.map(i => <li>{i}</li>);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          placeholder="Separate items with commas" /><br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My To Do List:</h1>
+          <ul>
+            {items}
+          </ul>
       </div>
     );
   }
