63cde56da37ba4f721a62ad43b6790a0910709b9
Valentina Versari <versavale@gmail.com>
2018-10-26 07:58:47 +0200

    added reset button and counter

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ class App extends Component {
     super(props);
 this.state = {
   userInput: '',
-  toDoList: []
+  toDoList: [],
+  doneItems: 0
 }
 this.handleSubmit = this.handleSubmit.bind(this);
 this.handleChange = this.handleChange.bind(this);
+this.clearAll = this.clearAll.bind(this);
+this.checkboxes = this.checkboxes.bind(this);
   }
   handleSubmit(){
     const itemsArray = this.state.userInput.split(', ');
@@ -22,9 +25,30 @@ this.handleChange = this.handleChange.bind(this);
       userInput: e.target.value
     });
   }
+  clearAll(){
+    this.setState({
+      userInput: '',
+      toDoList: [],
+      doneItems: 0
+    })
+  }
+  checkboxes(){
+    let inputElems = document.getElementsByTagName("input"),
+    count = 0;
+      for (var i = 0; i < inputElems.length; i++) {       
+        if (inputElems[i].checked === true){
+          count++;
+          this.setState({
+            doneItems: count
+          });
+        }
+      }
+    }
+
   render() {
-    const items = this.state.toDoList.map(i => <li>{i}</li>);
-    const countItems = this.state.toDoList.length;
+    const items = this.state.toDoList.map(i => <div className="checkbox"><input type="checkbox" id={this.state.toDoList.indexOf(i)} onClick={this.checkboxes} /><label htmlFor={this.state.toDoList.indexOf(i)}>{i}</label></div>);
+    const doneCount = this.state.doneItems;
+    const countItems = this.state.toDoList.length - doneCount;
     return (
       <div className="App">
         <h1>My To-Do List</h1>
@@ -35,6 +59,7 @@ this.handleChange = this.handleChange.bind(this);
           value={this.state.userInput}
           placeholder="Add your items here, separated by commas" /><br />
         < div className="button-wrapper">
+        <button className="reset" onClick={this.clearAll}>Clear List</button>
         <button className="create" onClick={this.handleSubmit}>Create List</button>
         </div>
         </div>
@@ -45,9 +70,12 @@ this.handleChange = this.handleChange.bind(this);
           </ul>
         </div>
         <div className="action-wrapper">
-        <div className="action">
-        <p>To-Do: {countItems}</p>
-        </div>
+          <div className="action completed">
+            <p>Completed: {doneCount}</p>
+          </div>
+          <div className="action todo">
+            <p>To-Do: {countItems}</p>
+          </div>
         </div>
         </div>
       </div>
